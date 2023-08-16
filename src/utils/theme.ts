3b149cf445b29f8ca46f1d55fa2cf85d154bf7de
Fernando Plaza <fernando.plaza@itrocksweb.com>
2023-08-16 11:40:00 -0300

    create theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,28 @@
+
+export const theme = {
+    colors: {
+        green: "#08FB06",
+        whithe: "#FAFAFA",
+        black: "#3A3748",
+        gray: "#75737D",
+        grayLight: "#F2F1F4"
+    },
+    fonts: {
+        size: {
+            xxl: "126px",
+            xl: "88px",
+            lg: "72px",
+            md: "24px",
+            sm: "16px",
+            xs: "14px"
+        },
+        weight: {
+            xxl: "1000",
+            xl: "800",
+            lg: "700",
+            md: "600",
+            sm: "500",
+            xs: "400"
+        }
+    },
+}
