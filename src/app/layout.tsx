828242c6a4050920cb90872c0fe23b178ffc54cd
JYachelini <juliyachedev@gmail.com>
2023-08-17 15:09:53 -0300

    body margin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={mulish.className} style={{'marginBottom': '200px'}}>
+      <body className={mulish.className} style={{'margin': '0 0 200px 0'}}>
         <WagmiConfig config={config}>
           <LensProvider config={lensConfig}>
             {children}
